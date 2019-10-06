5271ddfb8d2858bb501443538c86fecf61e7ea03
pzenie <pzenie@users.noreply.github.com>
2019-10-05 22:42:58 -0400

    Initial commit

---
+++ Sudoku Solver/Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Sudoku_Solver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
