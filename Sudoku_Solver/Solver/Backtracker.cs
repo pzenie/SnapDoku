34b96e3283576124971e69d7a5ccade403d273f6
pzenie <pzenie@users.noreply.github.com>
2020-01-31 21:15:43 -0500

    Fixes for backtracker, pretty much completely working now

--- Sudoku_Solver/Solver/Backtracker.cs
+++ Sudoku_Solver/Solver/Backtracker.cs
@@ -1,165 +1,118 @@
 ﻿using Sudoku_Solver.Data;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sudoku_Solver.Solver
 {
     internal static class Backtracker
     {
-         public static BoardModel BackTrack(BoardModel board, List<List<List<Tuple<int, int>>>> groups)
-         {
-             if(board.currentX == -1 && board.currentY == -1)
-             {
-                 return board;
-             }
-             Cell currentCell = board.BoardValues[board.currentX][board.currentY];
-             if (currentCell.GetPossibleValues().Count == 0) return null;
-             List<int> values = new List<int>(currentCell.GetPossibleValues());
-             foreach (int value in values)
-             {
-                 BoardModel tempBoard = new BoardModel(board);
-                 tempBoard.BoardValues[tempBoard.currentX][tempBoard.currentY].CellValue = value;
-                 //Pruner.PrunePuzzle(tempBoard, groups);
-                 var nextCords = GetNextCoordinates(tempBoard.currentX, tempBoard.currentY, tempBoard.BoardValues.Length);
-                 int x = nextCords.Item1;
-                 int y = nextCords.Item2;
-                 SkipFilled(tempBoard, currentCell, x, y);
-                 tempBoard = BackTrack(new BoardModel(tempBoard), groups);
-                 if (tempBoard != null)
-                 {
-                     board = tempBoard;
-                     break;
-                 }
-             }
-             return board;
-         }
-         public static BoardModel BackTrack1(BoardModel board, List<List<List<Tuple<int, int>>>> groups)
-         {
-             Stack<BoardModel> boardStack = new Stack<BoardModel>();
-             boardStack.Push(new BoardModel(board));
-             DateTime timeout = DateTime.Now.AddMilliseconds(10000);
-             while (boardStack.Count > 0 && DateTime.Now < timeout)
-             {
-                 BoardModel currentBoard = boardStack.Pop();
-                 Tuple<int, int> coords = GetNextCoordinates(currentBoard.currentX, currentBoard.currentY, currentBoard.BoardValues.Length);
-                 int nextX = coords.Item1;
-                 int nextY = coords.Item2;
-                 Cell currentCell = currentBoard.BoardValues[currentBoard.currentX][currentBoard.currentY];
-                 if (nextX == -1 || nextY == -1) // End of board
-                 {
-                     if (currentCell.CellValue == 0)
-                     {
-                         if (currentCell.GetPossibleValues().Count == 1) // should only be one option if valid, 0 if not
-                         {
-                             currentCell.CellValue = currentCell.GetPossibleValues()[0];
-                             return currentBoard;
-                         }
-                     }
-                     else
-                     {
-                         return currentBoard;
-                     }
-                 }
-                 else
-                 {
-                     //Pruner.PrunePuzzle(currentBoard, groups);
-                     PushNextBoards(boardStack, currentBoard, currentCell, nextX, nextY);
-                 }
-             }
-             return board;
-         }
+        public static int[][] BackTrack(int[][] board, BitArray[][] possibleValues, Tuple<int, int> curLocation,
+                                           List<List<List<Tuple<int, int>>>> groups)
+        {
+            if (curLocation.Item1 == -1 && curLocation.Item2 == -1)
+            {
+                return board;
+            }
+            if (NoPossibleValuesForAll(board, possibleValues)) return null;
+            bool solutionFound = false;
+            for (int i = 0; i < possibleValues[curLocation.Item1][curLocation.Item2].Length; i++)
+            {
+                if (possibleValues[curLocation.Item1][curLocation.Item2][i])
+                {
+                    var newBoard = DeepCopyIntArray(board);
+                    var newPossibleValues = DeepCopyBitArray(possibleValues);
+                    newBoard[curLocation.Item1][curLocation.Item2] = i + 1;
+                    newBoard = Pruner.PrunePuzzle(newBoard, groups);
+                    Tuple<int, int> nextEmptyCell = SkipFilled(newBoard, new Tuple<int, int>(curLocation.Item1, curLocation.Item2));
+                    newPossibleValues = Pruner.PruneAllCells(newBoard, newPossibleValues, groups);
+                    newBoard = BackTrack(newBoard, newPossibleValues, nextEmptyCell, groups);
+                    if (newBoard != null)
+                    {
+                        board = newBoard;
+                        solutionFound = true;
+                        break;
+                    }
+                }
+            }
+            if (solutionFound) return board;
+            else return null;
+        }
 
-         private static void PushNextBoards(Stack<BoardModel> boardStack, BoardModel currentBoard, Cell currentCell, int nextX, int nextY)
-         {
-             SkipFilled(currentBoard, currentCell, nextX, nextY);
-             if (currentCell.CellValue != 0)
-             {
-                 boardStack.Push(currentBoard);
-             }
-             else
-             {
-                 if (AllCellsHavePossibleValues(currentBoard))
-                 {
-                     foreach (int value in currentCell.GetPossibleValues())
-                     {
-                         BoardModel nextBoard = new BoardModel(currentBoard);
-                         nextBoard.BoardValues[currentBoard.currentX][currentBoard.currentY].CellValue = value;
-                         nextBoard.BoardValues[currentBoard.currentX][currentBoard.currentY].RemovePossibleValue(value);
-                         boardStack.Push(nextBoard);
-                     }
-                 }
-             }
-         }
+        private static bool NoPossibleValuesForAll(int[][] board, BitArray[][] possibleValues)
+        {
+            for(int i = 0; i < board.Length; i++)
+            {
+                for(int j = 0; j < board[i].Length; j++)
+                {
+                    if(board[i][j] == 0)
+                    {
+                        if (!possibleValues[i][j].Cast<bool>().Contains(true))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        private static int[][] DeepCopyIntArray(int[][] board)
+        {
+            int[][] newBoard = new int[board.Length][];
+            for(int i = 0; i < board.Length; i++)
+            {
+                newBoard[i] = new int[board[i].Length];
+                for(int j = 0; j < board[i].Length; j++)
+                {
+                    newBoard[i][j] = board[i][j];
+                }
+            }
+            return newBoard;
+        }
 
-         private static bool AllCellsHavePossibleValues(BoardModel board)
-         {
-             for (int i = 0; i < board.BoardValues.Length; i++)
-             {
-                 for (int j = 0; j < board.BoardValues[i].Length; j++)
-                 {
-                     Cell currentCell = board.BoardValues[i][j];
-                     if (currentCell.CellValue == 0 && currentCell.GetPossibleValues().Count == 0)
-                     {
-                         return false;
-                     }
-                 }
-             }
-             return true;
-         }
+        private static BitArray[][] DeepCopyBitArray(BitArray[][] possibleValues)
+        {
+            BitArray[][] newPossibleValues = new BitArray[possibleValues.Length][];
+            for(int i = 0; i < possibleValues.Length; i++)
+            {
+                newPossibleValues[i] = new BitArray[possibleValues[i].Length];
+                for(int j = 0; j < possibleValues[i].Length; j++)
+                {
+                    newPossibleValues[i][j] = new BitArray(possibleValues[i][j].Length, false);
+                    for(int k = 0; k < possibleValues[i][j].Length; k++)
+                    {
+                        newPossibleValues[i][j][k] = possibleValues[i][j][k];
+                    }
+                }
+            }
+            return newPossibleValues;
+        }
 
-         private static void SkipFilled(BoardModel currentBoard, Cell currentCell, int nextX, int nextY)
-         {
-             while (currentCell.CellValue != 0)
-             {
-                 Tuple<int, int> res = GetNextCoordinates(nextX, nextY, currentBoard.BoardValues.Length);
-                 nextX = res.Item1;
-                 nextY = res.Item2;
-                 currentBoard.currentX = nextX;
-                 currentBoard.currentY = nextY;
-                 if(nextX == -1 && nextY == -1)
-                 {
-                     break;
-                 }
-                 else
-                 {
-                     currentCell = currentBoard.BoardValues[nextX][nextY];
-                 }
-             }
-         }
+        public static Tuple<int,int> SkipFilled(int[][] board, Tuple<int,int> location)
+        {
+            while (board[location.Item1][location.Item2] != 0)
+            {
+                location = GetNextCoordinates(location.Item1, location.Item2, board.Length);
+                if (location.Item1 == -1 && location.Item2 == -1) break;
+            }
+            return location;
+        }
 
-         private static void SkipFilled1(BoardModel currentBoard, Cell currentCell, int nextX, int nextY)
-         {
-             while (currentCell.CellValue != 0)
-             {
-                 Tuple<int, int> res = GetNextCoordinates(nextX, nextY, currentBoard.BoardValues.Length);
-                 nextX = res.Item1;
-                 nextY = res.Item2;
-                 if (nextX > -1 && nextY > -1)
-                 {
-                     currentCell = currentBoard.BoardValues[nextX][nextY];
-                     currentBoard.currentX = nextX;
-                     currentBoard.currentY = nextY;
-                 }
-                 else
-                 {
-                     break;
-                 }
-             }
-         }
-         private static Tuple<int, int> GetNextCoordinates(int x, int y, int size)
-         {
-             if (y + 1 < size)
-             {
-                 return new Tuple<int, int>(x, y + 1);
-             }
-             else if (x + 1 < size)
-             {
-                 return new Tuple<int, int>(x + 1, 0);
-             }
-             else
-             {
-                 return new Tuple<int, int>(-1, -1);
-             }
-         }
+        private static Tuple<int, int> GetNextCoordinates(int x, int y, int size)
+        {
+            if (y + 1 < size)
+            {
+                return new Tuple<int, int>(x, y + 1);
+            }
+            else if (x + 1 < size)
+            {
+                return new Tuple<int, int>(x + 1, 0);
+            }
+            else
+            {
+                return new Tuple<int, int>(-1, -1);
+            }
+        }
     }
 }
--- Sudoku_Solver/Solver/Solver.cs
+++ Sudoku_Solver/Solver/Solver.cs
@@ -9,7 +9,9 @@ namespace Sudoku_Solver.Solver
         public static int[][] PuzzleSolver(int[][] board, List<List<List<Tuple<int, int>>>> groups)
         {
             board = Pruner.PrunePuzzle(board, groups);
-            //board = Backtracker.BackTrack(board, groups);
+            var possibleValues = Pruner.PruneAllCells(board, Pruner.InitPossibleValues(board), groups);
+            var location = Backtracker.SkipFilled(board, new Tuple<int, int>(0, 0));
+            board = Backtracker.BackTrack(board, possibleValues, location, groups);
             return board;
         }
     }
